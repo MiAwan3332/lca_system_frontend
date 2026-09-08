21e39e6fd7bd1d7b82a7c4418dacec4eeb9f40ca
MiAwan'sDev <103881058+MiAwan3332@users.noreply.github.com>
2026-09-08 09:18:42 +0500

    fixes into fee slip margin top

--- src/utlls/feeSlipLayout.js
+++ src/utlls/feeSlipLayout.js
@@ -8,7 +8,7 @@ export const SLIP_WIDTH_MM = SLIP_WIDTH_IN * 25.4;
 export const SLIP_HEIGHT_MM = SLIP_HEIGHT_IN * 25.4;
 
 /** Print margins on 5×7 paper (applied when printing fee / admission slips). */
-export const SLIP_PRINT_MARGIN_TOP_IN = 1.5;
+export const SLIP_PRINT_MARGIN_TOP_IN = 1;
 export const SLIP_PRINT_MARGIN_BOTTOM_IN = 1;
 export const SLIP_PRINT_MARGIN_TOP_MM = SLIP_PRINT_MARGIN_TOP_IN * 25.4;
 export const SLIP_PRINT_MARGIN_BOTTOM_MM = SLIP_PRINT_MARGIN_BOTTOM_IN * 25.4;
